6d5b0e6c62cb74610d3e959a43ec316185a679d1
DanielLab <danillp@gmail.com>
2026-03-18 07:46:13 -0600

    act

---
+++ Projects/ESP32_OLEd/src/main.cpp
@@ -0,0 +1,34 @@
+#include<Arduino.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 32 // Ajustado a tu pantalla
+#define OLED_RESET    -1 
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void setup() {
+  // Inicializa I2C en pines 8 (SDA) y 9 (SCL)
+  Wire.begin(); 
+
+  // Dirección I2C típica: 0x3C
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+    for(;;); // Bloquea si no encuentra la pantalla
+  }
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0,0);
+  display.println("ESP32+SSD1306");
+  display.println("Tamanio 1");
+  display.println("OLED 128x32 OK!");
+  display.println("Practica");
+  display.display();
+}
+
+void loop() {
+  // Tu código aquí
+}
