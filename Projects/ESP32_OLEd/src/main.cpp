0079aae2bf35db4abe9a272ab7e33ab92cc59522
DanielLab <danillp@gmail.com>
2026-06-12 10:52:04 -0600

    Cambio de 128x32 a 128x64

--- Projects/ESP32_OLEd/src/main.cpp
+++ Projects/ESP32_OLEd/src/main.cpp
@@ -4,7 +4,7 @@
 #include <Adafruit_SSD1306.h>
 
 #define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 32 // Ajustado a tu pantalla
+#define SCREEN_HEIGHT 64 // Ajustado a tu pantalla
 #define OLED_RESET    -1 
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
